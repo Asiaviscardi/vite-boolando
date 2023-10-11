e153c2d1067b58ae5323e56f01084f02bb9e0c0b
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-11 14:49:34 +0200

    trasferimento header

--- src/components/icons/Header.vue
+++ src/components/icons/Header.vue
@@ -7,10 +7,83 @@ export default {
 
 <template>
 
-    <h2>header</h2>
+<div class="barra flex">
+
+<div id="categorie pad">
+
+    <ul class="flex">
+
+        <li><a href="">Donna</a></li>
+
+        <li><a href="">Uomo</a></li>
+
+        <li><a href="">Bambini</a></li>
+
+    </ul>
+
+</div>
+
+<div class="lb pad flex">
+
+    <img id="logo" src="img/boolean-logo.png" alt="">
+
+</div>
+
+<div class="funzioni pad">
+
+    <i class="fa-regular fa-user" style="color: #ffffff;"></i>
+
+    <i class="fa-regular fa-heart" style="color: #ffffff;"></i>
+
+    <i class="fa-solid fa-bag-shopping" style="color: #ffffff;"></i>
+
+    
+</div>
+
+</div>
   
 </template>
 
 <style scoped lang="scss">
+
+a{
+    text-decoration: none;
+    color: white;
+    
+}
+
+li{
+    list-style: none;
+    margin-right: 7px;
+}
+
+.barra{
+    width: 100%;
+    height: 50px;
+    background-color: #ff6900;
+    border-bottom: 2px solid rgba(169, 169, 169, 0.5);
+}
+
+.flex{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+.lb{
+    align-items: center;
+}
+
+#logo{
+    width: 100px;
+}
+
+.funzioni i{
+    cursor: pointer;
+    margin-right: 3px;
+    
+}
+
+
 
 </style>
